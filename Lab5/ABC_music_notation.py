60d24a82c008f161458ed6ed986b984fc6a6e3fa
Karl-Finn <56453075+Karl-Finn@users.noreply.github.com>
2019-10-24 20:52:57 +0100

    Update ABC_music_notation.py

--- Lab5/ABC_music_notation.py
+++ Lab5/ABC_music_notation.py
@@ -5,16 +5,23 @@ filename = 'hnr1.abc'
 with open(filename, 'r') as f:
     X, T, M, K = "", "", "", ""
     for line in f:
-        if line[0] == "X":
+        if line[:2] == "X:":
+            if X:
+                X, T, M, K = "", "", "", ""
             X = line[2:-1]
-        elif line[0] == "T":
+        elif line[:2] == "T:":
             if T: #as the are multiple T lines in each block (and we only want the first in each) this checks if T is already assigned
                 continue #If it is, then we go back to line 7 and continue with the next line in the file!
             else:
                 T = line[2:-1]
-        elif line[0] == "M":
+        elif line[:2] == "M:":
             M = line[2:-1]
-        elif line[0] == "K":
+        elif line[:2] == "K:":
             K = line[2:-1]
             print("{0} ... {1} ... Time sig: {2} ... Key sig: {3}".format(X, T, M, K))
-            X, T, M, K = "", "", "", ""
+            # X, T, M, K = "", "", "", ""
+
+# originally lines 9 and 10 were not in the program 
+# this won't work correctly if, by chance, K: comes before M:
+# if this happens then all variables will be wiped (re-initialized as empty strings)
+# ***added lines 7 and 8 to fix it*** 
