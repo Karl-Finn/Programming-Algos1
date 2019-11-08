fa6416cad1e2e97646c8ff293ff887c2d5309105
Karl-Finn <56453075+Karl-Finn@users.noreply.github.com>
2019-11-08 01:08:38 +0000

    Update theGettysburgAddress.py

--- Lab6/theGettysburgAddress.py
+++ Lab6/theGettysburgAddress.py
@@ -3,25 +3,23 @@ import requests
 # 1. RETRIEVE THE SPEECH FROM THE WEB
 speech_retrieve = requests.get("http://193.1.33.31:88/pa1/gettysburg.txt")
 speech_headers = speech_retrieve.headers
-body = speech_retrieve.text  # .text produces the content as <str>
+body = speech_retrieve.text  # '.text' produces the content as <str>
 
 # 2. FORMAT THE SPEECH CONTENTS TO SOMETHING WE CAN WORK WITH (will result in 1 long sentence)
-# speech_lower = body.lower() # converts all letters to lower case
-# speech_no_period = speech_lower.replace(".", "") # removes all full-stops by replacing them with "" (i.e. nothing)
-# speech_no_comma = speech_no_period.replace(",", "") # removes all commas by replacing them with ""
-# speech_no_el = speech_no_comma.replace("\n\n", " ") # removes the end-line characters
-
-# NB: Lines 8, 9, 10, 11 can all be done with the following
-speech_formatted = body.lower().replace(".", "").replace(",", "").replace("-", " ").replace("\n\n", " ")
+speech_formatted = body.lower()\
+    .replace(".", "")\
+    .replace(",", "")\
+    .replace("-", " ")\
+    .replace("\n\n", " ")
 speech_word_list = speech_formatted.split()
 
-# 3. RETRIEVE THE STOPWORDS FROM THE WEB
+# 3. RETRIEVE THE STOPWORDS FROM THE WEB & CREATE A LIST OF EACH WORD
 stopwords_retrieve = requests.get("http://193.1.33.31:88/pa1/stopwords.txt")
 stopwords_headers = stopwords_retrieve.headers
 stopwords = stopwords_retrieve.text
-stopwords_list = stopwords.split(sep=",")
+stopwords_list = stopwords.split(sep=",") # creates a list of each word
 
-# 4. CREATE A LIST OF WORDS FROM THE SPEECH THAT DOES NOT INCLUDE ANY OF THE STOP-WORDS
+# 4. CREATE A LIST OF UNIQUE WORDS (WORDS IN SPEECH MINUS ANY STOP-WORDS)
 unique_words = [word for word in speech_word_list if word not in stopwords_list] # list comprehension
 
 # 5. CREATE A DICTIONARY THAT COUNTS HOW MANY TIMES A UNIQUE WORD APPEARS IN THE SPEECH
@@ -46,3 +44,4 @@ for k, v in unique_words_dict.items():
     else: # on the 10th key-value pair 'count' be re-initialized to 1
         count = 1
         print(f"{k}: {v}") # and we print the 10th key-value pair for that row
+
