7d203bbd1156f6537254c3bd93d66ade4344e040
Karl-Finn <56453075+Karl-Finn@users.noreply.github.com>
2019-11-12 20:03:11 +0000

    Update my_functions.py

--- my_functions.py
+++ my_functions.py
@@ -4,13 +4,14 @@ def format_txt_file_as_list(file):
         Note: hyphened words will NOT be treated as distinct words.
             The hyphen will be removed and the words will be taken seperately.
         """
-    file_formatted = file.lower()\
-        .replace(",", "")\
-        .replace("-", " ")\
-        .replace(".", "")\
+    file_formatted = file.lower() \
+        .replace(",", "") \
+        .replace("-", " ") \
+        .replace(".", "") \
         .replace("\n\n", " ")
     word_list = file_formatted.split()
     return word_list
+
 
 def get_text_file_from_net(text_url):
     """Takes a URL of a text file that is on the web as
@@ -24,28 +25,27 @@ def get_text_file_from_net(text_url):
     except Exception as err:
         print(f"Error occurred: {err}")
 
-def uniqueWordsList(checkList, excludeList):
-    """Takes two lists of words as input.
 
+def unique_words_list(checklist, exclude_list):
+    """Takes two lists of words as input.
         Param 1: a list of words which will be checked against Param 2
         Param 2: a second list of words
-
         Output: A list of words from Param 1 that do NOT appear in Param 2
     """
-    unique = [word for word in checkList if word not in excludeList]
+    unique = [word for word in checklist if word not in exclude_list]
     return unique
 
 
-def wordHist(wordList):
+def word_hist(word_list):
     """
-
-    :param wordList: a list of words
+    :param word_list: a list of words
     :return: a dictionary histogram of each word and how many times it appears in wordList
     """
     hist = dict()
-    for word in wordList:
+    for word in word_list:
         if word not in hist:
             hist[word] = 1
         else:
             hist[word] += 1
     return hist
+
